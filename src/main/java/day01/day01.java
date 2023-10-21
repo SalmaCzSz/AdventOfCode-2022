65459e768ff584cc081083a74c3fb49ccfbe72dc
Salma Cruz Sánchez <salma.cruz@fygdevelopers.com>
2023-10-20 19:27:32 -0600

    20/10/2023 | Day 1 Calorie Counting

---
+++ src/main/java/day01/day01.java
@@ -0,0 +1,15 @@
+package day01;
+
+/**
+ *
+ * @author Salma
+ */
+public class day01 {
+
+    public static void main(String[] args) {
+        functions funcion = new functions();
+        funcion.leerArchivo();
+        funcion.calcularMayor();
+        funcion.calculaTresMayores();
+    }
+}
---
+++ src/main/java/day01/functions.java
@@ -0,0 +1,79 @@
+package day01;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.stream.Collector;
+
+/**
+ *
+ * @author Salma
+ */
+public class functions {
+    ArrayList<Integer> elfo = new ArrayList<>();
+    int caloriasMax = 0;
+    
+    public void leerArchivo(){
+        String dato;
+        int caloriasElfo = 0;
+        int totalElfos = 0;
+        
+        try{
+            /* apertura del archivo */
+            FileReader archivo = new FileReader("C:\\Users\\Salma\\Documents\\GitHub\\AdventOfCode-2022\\src\\main\\java\\day01\\registro-calorias.txt"); 
+            BufferedReader lee = new BufferedReader(archivo);
+            
+            /* lectura del archivo */
+            if(lee.ready()){
+                // System.out.println("" + dato.toString());
+                while((dato = lee.readLine()) != null){
+                    if(!dato.isEmpty()){
+                        caloriasElfo += Integer.parseInt(dato);
+                    } else {
+                        elfo.add(caloriasElfo);
+                        caloriasElfo = 0; 
+                        totalElfos += 1;
+                    }
+                }
+                
+                System.out.println("leerArchivo | Total elfos: " + totalElfos);
+            }
+        } catch(IOException | NumberFormatException e){
+            System.err.println("leerArchivo | Error: " + e);
+        }
+    }
+    
+    public void calcularMayor(){
+        caloriasMax = Collections.max(elfo);
+        int numElfo = 0;
+        
+        for (int i = 0; i < elfo.size(); i++) {
+            if(elfo.get(i) == caloriasMax ){
+                numElfo = i + 1;
+            }
+        }
+        
+        System.out.println("El elfo que lleva más calorias es el " + numElfo + " (" + caloriasMax + " calorias).");
+    }   
+    
+    public void calculaTresMayores(){
+        int totalCalorias = 0;
+        /* ordenamos la lista */
+        elfo.sort(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o2.compareTo(o1);
+            }
+        });
+        
+        /* sumamos calorias */
+        for(int i = 0 ; i < 3; i++){
+            totalCalorias += elfo.get(i);
+        }
+        
+        System.out.println("Los tres elfos con más calorías suman " + totalCalorias + " calorias");
+    }
+}
